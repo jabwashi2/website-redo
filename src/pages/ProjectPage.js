32144f3836b7394c8810c6bef3395d9362f8d5a0
Jabrecia Washington <jabwashi2@gmail.com>
2024-12-12 01:12:02 -0500

    creating project specific pages is possible now; next, format pages appropriately to match figma

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -1,13 +1,28 @@
 import { useParams } from 'react-router-dom'
+import dataJSON from '../content.json'
 // import {ProjectTemplate} from '../components/ProjectTemplate'
 
-export function ProjectPage(props) {
+export function ProjectPage() {
 
     const {id} = useParams();
+    let project = dataJSON.projects.find(project => project.id === id);
 
     return (
         <>
-            <p>{id}</p>
+            <div id={id}>
+                <h1>{project.title}</h1>
+                {project.tools.map((tool) => <p>{tool}</p>)}
+                {/* <p>There will be an image carousel here</p> */}
+                <br></br>
+                
+                {/* <h1>Breakdown</h1>
+                <h2>Lower Header 1</h2>
+                <p>Describe stuff here</p>
+                <h2>Lower Header 2</h2>
+                <p>Describe stuff here</p>
+                <h2>Lower Header 3</h2>
+                <p>Describe stuff here</p> */}
+            </div>
         </>
     )
 }
