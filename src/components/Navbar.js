b418cecd00baf6ab87e618ddebcea782eface608
Jabrecia Washington <jabwashi2@gmail.com>
2025-01-03 22:49:27 -0500

    working on menu icon (still won't show)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,10 @@ import { Link } from "react-router-dom"
 
 export function Navbar() {
     return (
-        <>
-        <nav expand="lg">
+        <nav expand="navbar navbar-expand-lg navbar-light bg-steel">
             <div className="container-fluid">
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                <button class="btn" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -27,6 +26,5 @@ export function Navbar() {
                 </div>
             </div>
         </nav>
-        </>
     );
 }
